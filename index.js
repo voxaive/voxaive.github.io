1752dc326a3b54c705eb00f71dd13dca138c3250
voxaive <148632238+voxaive@users.noreply.github.com>
2024-01-05 23:33:19 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const nav = document.querySelector('.BarraN');
-const nav2 = document.querySelector('.cajaMenuRep');
-window.addEventListener('scroll', function(){
-    nav.classList.toggle('active', window.scrollY>0)
-    nav2.classList.toggle('active', window.scrollY>0)
-})
-
